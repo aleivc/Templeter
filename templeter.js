bff843e06cbf733f23420a92bbbe5006daed1176
aleivc <aleivc@163.com>
2019-04-24 11:29:29 +0800

    update diff && remove

--- templeter.js
+++ templeter.js
@@ -1,14 +1,20 @@
 window.onload = function(){
     var tagName = document.getElementsByTagName('script')[0].getAttribute('tag');
-    console.log(tagName);
     if(tagName == 'common'){
         var ohtml = document.getElementsByTagName('*');
+        var arr1 = [];
         for(var i = 0; i < ohtml.length; i++){
             var index = i;
             var str = ohtml[i].tagName;
             if(str.indexOf('-') != -1){
                 localStorage[str] = ohtml[index].innerHTML;
+                arr1.push(str);
             }
+        }
+        var arr2 = Object.keys(localStorage);
+        var different = getArrDifference(arr1, arr2);
+        for(var k = 0; k < different.length; k++){
+            localStorage.removeItem(different[k]);
         }
     }
     else{
@@ -28,5 +34,11 @@ window.onload = function(){
                 this.append(div);
             }
         }
+    }
+
+    function getArrDifference(arr1, arr2){
+        return arr1.concat(arr2).filter(function(v, i, arr){
+            return arr.indexOf(v) === arr.lastIndexOf(v);
+        })
     }
 }

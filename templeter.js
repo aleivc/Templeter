7256bdb61590fe7653267a732796c2026215a526
leihonger <aleivc@163.com>
2021-04-29 09:40:21 +0800

    es5 => es6

--- templeter.js
+++ templeter.js
@@ -1,44 +1,43 @@
-window.onload = function(){
-    var tagName = document.getElementsByTagName('script')[0].getAttribute('tag');
-    if(tagName == 'common'){
-        var ohtml = document.getElementsByTagName('*');
-        var arr1 = [];
-        for(var i = 0; i < ohtml.length; i++){
-            var index = i;
-            var str = ohtml[i].tagName;
-            if(str.indexOf('-') != -1){
-                localStorage[str] = ohtml[index].innerHTML;
-                arr1.push(str);
-            }
-        }
-        var arr2 = Object.keys(localStorage);
-        var different = getArrDifference(arr1, arr2);
-        for(var k = 0; k < different.length; k++){
-            localStorage.removeItem(different[k]);
-        }
+window.onload = function () {
+  const tagName = document.getElementsByTagName('script')[0].getAttribute('tag');
+  if (tagName === 'common') {
+    const html = document.getElementsByTagName('*');
+    const arr1 = [];
+    for (let i = 0; i < html.length; i++) {
+      const index = i;
+      const str = html[i].tagName;
+      if (str.indexOf('-') !== -1) {
+        localStorage[str] = html[index].innerHTML;
+        arr1.push(str);
+      }
     }
-    else{
-        var arrs = Object.keys(localStorage);
-        for(var j = 0; j < arrs.length; j++){
-            var tmp = arrs[j];
-            var temp = localStorage[tmp];
-            customElements.define(tmp.toLowerCase(), getClass(temp));
-        }
+    const arr2 = Object.keys(localStorage);
+    const different = getArrDifference(arr1, arr2);
+    for (let k = 0; k < different.length; k++) {
+      localStorage.removeItem(different[k]);
     }
-    function getClass(a){
-        return class me extends HTMLElement{
-            constructor(){
-                super();
-                var div = document.createElement('div');
-                div.innerHTML = a;
-                this.append(div);
-            }
-        }
+  } else {
+    const ars = Object.keys(localStorage);
+    for (let j = 0; j < ars.length; j++) {
+      const tmp = ars[j];
+      const temp = localStorage[tmp];
+      customElements.define(tmp.toLowerCase(), getClass(temp));
     }
+  }
+  function getClass(a) {
+    return class me extends HTMLElement {
+      constructor() {
+        super();
+        const div = document.createElement('div');
+        div.innerHTML = a;
+        this.append(div);
+      }
+    };
+  }
 
-    function getArrDifference(arr1, arr2){
-        return arr1.concat(arr2).filter(function(v, i, arr){
-            return arr.indexOf(v) === arr.lastIndexOf(v);
-        })
-    }
-}
+  function getArrDifference(arr1, arr2) {
+    return arr1.concat(arr2).filter(function (v, i, arr) {
+      return arr.indexOf(v) === arr.lastIndexOf(v);
+    });
+  }
+};
